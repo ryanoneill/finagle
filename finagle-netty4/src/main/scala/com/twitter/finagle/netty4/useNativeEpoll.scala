a1984bbe27313d6b70bbd022eceddd619516b93f
Ryan O'Neill <ryano@twitter.com>
2019-12-10 15:01:58 -0800

    Set back to use native epoll

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/useNativeEpoll.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/useNativeEpoll.scala
@@ -23,9 +23,7 @@ import com.twitter.app.GlobalFlag
  * @see https://netty.io/wiki/native-transports.html
  */
 private object useNativeEpoll
-    // TODO: Undo -- This is temporary
-    // extends GlobalFlag[Boolean](true, "Use Linux's native epoll transport, when available") {
-    extends GlobalFlag[Boolean](false, "Use Linux's native epoll transport, when available") {
+    extends GlobalFlag[Boolean](true, "Use Linux's native epoll transport, when available") {
 
   @volatile private var overrideValue = Option.empty[Boolean]
 
